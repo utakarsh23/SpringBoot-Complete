f7bf525e0beacd272fef48aebf117dfda4e3355f
utakarsh23 <utakarsh.t@somaiya.edu>
2024-11-23 11:24:32 +0530

    authenticate Journal and also updated GetMapping(user journals) to be found through auth

--- src/main/java/com/example/crud/Controller/JournalEntryController.java
+++ src/main/java/com/example/crud/Controller/JournalEntryController.java
@@ -7,6 +7,8 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +24,10 @@ public class JournalEntryController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/{userName}")
-    public ResponseEntity<?> getAllJournalEntriesOfUser(@PathVariable String userName) {
+    @GetMapping
+    public ResponseEntity<?> getAllJournalEntriesOfUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User user = userService.findByUsername(userName);
         List<JournalEntry> all = user.getJournalEntries();
         if(all != null && !all.isEmpty()) {
@@ -32,7 +36,7 @@ public class JournalEntryController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/{userName}")
+    @PostMapping
     public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName) {
         try {
             journalEntryService.saveEntry(myEntry, userName);
